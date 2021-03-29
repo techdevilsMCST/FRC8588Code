acada3ed3f5d0bd82d63e43ebeb839ac353a5134
Tejas Mehta <tmthecoder@gmail.com>
2021-03-29 12:56:48 -0400

    Make abstractions for a tank drive subsystem (untested)

--- src/main/java/frc/team8588/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/team8588/subsystems/ExampleSubsystem.java
@@ -5,9 +5,28 @@
 
 package frc.team8588.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
-public class ExampleSubsystem implements Subsystem {
+public class ExampleSubsystem extends SubsystemBase
+{
+    /** Creates a new ExampleSubsystem. */
+    public ExampleSubsystem()
+    {
 
+    }
+
+
+    @Override
+    public void periodic()
+    {
+        // This method will be called once per scheduler run
+    }
+
+
+    @Override
+    public void simulationPeriodic()
+    {
+        // This method will be called once per scheduler run during simulation
+    }
 }
---
+++ src/main/java/frc/team8588/subsystems/drive/tank/TankDriveChassis.java
@@ -0,0 +1,18 @@
+/****
+ * Made by Tejas Mehta
+ * Made on Monday, March 29, 2021
+ * File Name: TankDriveChassis
+ * Package: frc.team8588.subsystems.drive.tank*/
+package frc.team8588.subsystems.drive.tank;
+
+import edu.wpi.first.wpilibj.PWMSparkMax;
+
+public class TankDriveChassis {
+
+    private PWMSparkMax left, right;
+
+    public TankDriveChassis(PWMSparkMax left, PWMSparkMax right) {
+        this.left = left;
+        this.right = right;
+    }
+}

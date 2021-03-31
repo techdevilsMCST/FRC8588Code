6face69068582a45aa04275bc621a1aa5d734be9
Tejas Mehta <tmthecoder@gmail.com>
2021-03-30 21:33:03 -0400

    Some arcade changes

--- src/main/java/frc/team8588/subsystems/drive/Arcade/ArcadeDriveChassis.java
+++ src/main/java/frc/team8588/subsystems/drive/Arcade/ArcadeDriveChassis.java
@@ -6,8 +6,7 @@ Check out ArcadeDriveSubsystem and other subsystems.
 Date: 3/29/2021
  */
 
-frc.team8588.subsystems.drive.Arcade;
-package frc.team8588.subsystems.drive.Arcade;
+package frc.team8588.subsystems.drive.arcade;
 
 import edu.wpi.first.wpilibj.PWMSparkMax;
 

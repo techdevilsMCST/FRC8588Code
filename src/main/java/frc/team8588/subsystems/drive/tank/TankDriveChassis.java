ff686e3c1b424d9e53e54deac32dc6a6869980e4
VirtualIce <56491625+CodeSample15@users.noreply.github.com>
2022-01-12 16:27:30 -0500

    Fixed typo that created errors with the arcade drive code. (The A in arcade for the package name wasn't capitalized)

--- src/main/java/frc/team8588/subsystems/drive/tank/TankDriveChassis.java
+++ src/main/java/frc/team8588/subsystems/drive/tank/TankDriveChassis.java
@@ -10,19 +10,25 @@ import edu.wpi.first.wpilibj.PWMSparkMax;
 
 public class TankDriveChassis {
 
-    private CANSparkMax left, right;
+    private CANSparkMax frontRight, frontLeft, backRight, backLeft;
 
-    public TankDriveChassis(CANSparkMax left, CANSparkMax right) {
-        this.left = left;
-        this.right = right;
+    public TankDriveChassis(CANSparkMax frontRight, CANSparkMax frontLeft, CANSparkMax backRight, CANSparkMax backLeft) {
+        this.frontRight = frontRight;
+        this.frontLeft = frontLeft;
+        this.backRight = backRight;
+        this.backLeft = backLeft;
 //        new CANSparkMax()
     }
 
-    public CANSparkMax getLeft() {
-        return left;
+    public CANSparkMax getFrontRight() {
+        return frontRight;
     }
 
-    public CANSparkMax getRight() {
-        return right;
+    public CANSparkMax getFrontLeft() {
+        return frontLeft;
     }
+
+    public CANSparkMax getBackRight() { return backRight; }
+
+    public CANSparkMax getBackLeft() { return backLeft; }
 }
